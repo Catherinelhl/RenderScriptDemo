a3aee1d66ed2fba90209e2ccaece066d6d030db9
Catherine.Brain William <15520054289@163.com>
2018-08-03 16:26:41 +0800

    通过缩小当前bitmap 的大小来实现优化，大概时间缩小了三倍作用

--- app/src/main/java/cn/catherine/renderscriptdemo/MainAty.kt
+++ app/src/main/java/cn/catherine/renderscriptdemo/MainAty.kt
@@ -18,24 +18,43 @@ class MainAty : AppCompatActivity() {
     private val handler = Handler {
         val bitmap = it.obj as Bitmap
         layout_one.background = BitmapDrawable(bitmap)
-
+        println("${it.arg1}:当前耗时：${System.currentTimeMillis() - currentTime}")
         false
     }
 
+    var count: Int = 0
+    private var currentTime: Long = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.aty_main)
-        Thread {
-            val bitmap = getBlurBitmap()
-            val mes = Message()
-            mes.obj = bitmap
-            handler.sendMessage(mes)
+        tv_dot.setOnClickListener {
+            currentTime = System.currentTimeMillis()
+            Thread {
+                val bitmap = when (count % 2) {
+                    0 -> getBlurBitmap(false)
+                    else -> getBlurBitmap(true)
 
-        }.start()
+                }
+                val mes = Message()
+                mes.obj = bitmap
+                mes.arg1 = count
+                handler.sendMessage(mes)
+                count++
+
+            }.start()
+        }
+
     }
 
-    private fun getBlurBitmap(): Bitmap {
+    private fun getBlurBitmap(isScale: Boolean): Bitmap {
         var bitmap = BitmapFactory.decodeResource(this.resources, R.drawable.icon)
+        var width = bitmap.width
+        var height = bitmap.height
+        if (isScale) {
+            width /= 8
+            height /= 8
+            bitmap = Bitmap.createScaledBitmap(bitmap, width, height, true)
+        }
         var renderScript = RenderScript.create(this)
 
         val scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript))
@@ -45,10 +64,11 @@ class MainAty : AppCompatActivity() {
 
         scriptIntrinsicBlur.setInput(input)
 
-        scriptIntrinsicBlur.setRadius(25.0f)
+        scriptIntrinsicBlur.setRadius(if (isScale) 1.0f else 25.0f)
         scriptIntrinsicBlur.forEach(output)
         output.copyTo(bitmap)
         renderScript.destroy()
         return bitmap
     }
+
 }
